068a5b0995bdf1133420a38d7a356f8bcfd31fe7
lizbryson <liz@lizbryson.co>
2023-06-06 16:11:56 -0600

    Styles and controls

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello!');
+let colorSelector = document.getElementById('logoColors');
+let containerSelector = document.getElementById('logoContainer');
+let logoArr = document.getElementsByClassName('customer-logo');
+
+colorSelector.addEventListener('change', (e) => {
+  let selectedColor = e.target.value;
+  [...logoArr].forEach((ele, idx) => {
+    ele.classList.remove('is-brand', 'is-grayscale', 'is-white');
+    ele.classList.add(`is-${selectedColor}`);
+  });
+});
+
+logoContainer.addEventListener('change', (e) => {
+  let containerChoice = e.target.value;
+  console.log(e.target.checked);
+  [...logoArr].forEach((ele, idx) => {
+    if (e.target.checked) {
+      ele.classList.add('is-contained');
+    } else {
+      ele.classList.remove('is-contained');
+    }
+  });
+});
